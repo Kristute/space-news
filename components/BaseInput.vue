3b56cd2c92660f41cf1133a882a9c79611503f96
Kristute <r.kryste@gmail.com>
2022-08-26 16:02:36 +0300

    Move title and input as custom reusable components, edit tailwind config for components files

---
+++ components/BaseInput.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="mb-4">
+        <label v-if="label">{{ label }}</label>
+        <input  
+        :placeholder="placeholder" 
+        :type="type" 
+        :value="value"
+        v-bind="$attrs"
+        class="shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+        <!-- @input="$emit('update:value', ($event.target as HTMLInputElement).value)" -->
+    </div>
+</template>
+<script setup lang="ts">
+defineProps({
+    label: {
+        type: [String, Boolean],
+        default: false,
+    },
+    placeholder: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    value: {
+        type: [String, Number],
+        default: '',
+    } 
+})
+
+// const updateValue = (event) => {
+//     return event.target.value;
+//     // this.$emit('update:value', event.target?.value)
+// }
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./components/**/*.ts",
+    "./components/**/*.vue",
     "./layouts/**/*.vue",
     "./pages/**/*.vue",
     './nuxt.config.js',
