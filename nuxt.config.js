75857f915ed0073ff8928a81bb1f21f7fbc5edbd
Kristute <r.kryste@gmail.com>
2022-08-31 10:42:01 +0300

    Add filter for date

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default defineNuxtConfig ({
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['@/plugins/filters.ts'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
---
+++ plugins/filters.ts
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+
+Vue.filter('formatDate', (value) => {
+  const date = new Date(value)
+  return date.toLocaleString(['en-US'], {
+    month: 'short',
+    day: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  })
+})
