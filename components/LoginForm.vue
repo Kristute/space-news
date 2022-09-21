d651f78a346044f9db93fc55dd140b025783e018
Kristute <r.kryste@gmail.com>
2022-09-21 10:10:39 +0300

    Add validation for username and password complexity

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -5,18 +5,67 @@
       novalidate
       @submit.prevent="onSubmit"
     >
+      <span
+        class="text-red-600"
+        :class="checkIsValidUsername ? 'hidden' : 'visible'"
+        >Username must contain only letters and numbers</span
+      >
       <BaseInput
         v-model="user.username"
         :placeholder="'Username'"
         :type="'text'"
         class="mb-4"
+        :class="{ 'border rounded border-red-500': !checkIsValidUsername }"
       />
-      <BaseInput
-        v-model="user.password"
-        :placeholder="'Password'"
-        :type="'password'"
-        class="mb-4"
-      />
+
+      <div class="relative">
+        <BaseInput
+          v-model="user.password"
+          :placeholder="'Password'"
+          :type="'password'"
+          autocomplete="off"
+          class="mb-4"
+        />
+        <div class="pb-3">Your password must contain:</div>
+        <ul class="bg-white p-3 rounded text-gray-500">
+          <PasswordCriteria
+            :text="'8 Characters'"
+            :is-criteria-accepted="checkMinimumLength"
+          />
+          <PasswordCriteria
+            :text="'Contains Number'"
+            :is-criteria-accepted="checkNumber"
+          />
+          <PasswordCriteria
+            :text="'Contains Uppercase'"
+            :is-criteria-accepted="checkUppercase"
+          />
+          <PasswordCriteria
+            :text="'Contains Lowercase'"
+            :is-criteria-accepted="checkLowerCase"
+          />
+          <PasswordCriteria
+            :text="'Contains Special Character'"
+            :is-criteria-accepted="checkSpecial"
+          />
+        </ul>
+
+        <div
+          class="checkmark_container"
+          :class="
+            checkIsValidPassword ? 'visible opacity-100' : 'hidden opacity-0'
+          "
+        >
+          <svg width="50%" height="50%" viewBox="0 0 140 100">
+            <path
+              class="checkmark"
+              :class="{ checked: checkIsValidPassword }"
+              d="M10,50 l25,40 l95,-70"
+            />
+          </svg>
+        </div>
+      </div>
+
       <PrimaryButton
         :disabled="!isDisabled"
         :type="'submit'"
@@ -29,6 +78,7 @@
 <script setup lang="ts">
 import { reactive, computed } from 'vue'
 import { User } from '../types/User'
+import PasswordCriteria from './PasswordCriteria.vue'
 
 const { $store } = useNuxtApp()
 const router = useRouter()
@@ -38,7 +88,80 @@ const user = reactive<User>({
   password: '',
 })
 
-const isDisabled = computed(() => user.username !== '' && user.password !== '')
+const checkIsValidUsername = computed(() => {
+  let isUsernameValid = ref(false)
+
+  isUsernameValid = ref(/^[A-Za-z0-9]*$/.test(user.username))
+
+  return isUsernameValid.value
+})
+
+const checkIsValidPassword = computed(() => {
+  let isValidPassword = ref(false)
+
+  if (
+    checkMinimumLength.value === true &&
+    checkNumber.value === true &&
+    checkUppercase.value === true &&
+    checkLowerCase.value === true &&
+    checkSpecial.value === true
+  ) {
+    isValidPassword = ref(true)
+  }
+
+  return isValidPassword.value
+})
+
+const checkMinimumLength = computed(() => {
+  let hasMinimumLength = ref(false)
+  if (user.password.length >= 8) {
+    hasMinimumLength = ref(true)
+  } else {
+    hasMinimumLength = ref(false)
+  }
+
+  return hasMinimumLength.value
+})
+
+const checkNumber = computed(() => {
+  let hasNumber = ref(false)
+
+  hasNumber = ref(/\d/.test(user.password))
+
+  return hasNumber.value
+})
+
+const checkUppercase = computed(() => {
+  let hasUppercase = ref(false)
+
+  hasUppercase = ref(/[A-Z]/.test(user.password))
+
+  return hasUppercase.value
+})
+
+const checkLowerCase = computed(() => {
+  let hasLowercase = ref(false)
+
+  hasLowercase = ref(/[a-z]/.test(user.password))
+
+  return hasLowercase.value
+})
+
+const checkSpecial = computed(() => {
+  const format = /[ `!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?~]/
+  let hasSpecial = ref(false)
+
+  hasSpecial = ref(format.test(user.password))
+
+  return hasSpecial.value
+})
+
+const isDisabled = computed(
+  () =>
+    user.password !== '' &&
+    checkIsValidUsername.value === true &&
+    checkIsValidPassword.value === true
+)
 
 const onSubmit = () => {
   $store.commit('auth/login', {
@@ -48,3 +171,39 @@ const onSubmit = () => {
   router.push({ name: 'articles' })
 }
 </script>
+<style>
+.checkmark_container {
+  border-radius: 50%;
+  position: absolute;
+  top: -15px;
+  right: -15px;
+  background: #2ecc71;
+  width: 50px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: opacity 0.4s ease;
+}
+
+.checkmark {
+  width: 100%;
+  height: 100%;
+  fill: none;
+  stroke: white;
+  stroke-width: 15;
+  stroke-linecap: round;
+  stroke-dasharray: 180;
+  stroke-dashoffset: 180;
+}
+
+.checked {
+  animation: draw 0.5s ease forwards;
+}
+
+@keyframes draw {
+  to {
+    stroke-dashoffset: 0;
+  }
+}
+</style>
