ebad9d767c07e6161747392c245b77653b1e83ff
Kristute <r.kryste@gmail.com>
2022-09-26 16:50:40 +0300

    Change <a> to <NuxtLink> to not refresh page, edit homepage link

--- components/Header.vue
+++ components/Header.vue
@@ -6,7 +6,7 @@
       <div
         class="flex flex-wrap justify-between items-center mx-auto w-full sm:w-4/5 max-w-screen-2xl"
       >
-        <a href="/" class="flex items-center">
+        <NuxtLink to="/articles" class="flex items-center">
           <img
             :src="rocketArrow"
             class="mr-3 h-6 sm:h-9"
@@ -16,7 +16,7 @@
             class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
             >Space News</span
           >
-        </a>
+        </NuxtLink>
         <div
           v-if="$store.state.auth.username"
           class="flex items-center lg:order-2"
