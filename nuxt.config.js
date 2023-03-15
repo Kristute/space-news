938849b2473ba322a62f99d951fb062408dc93b3
Kristute <r.kryste@gmail.com>
2023-03-15 09:02:18 +0200

    Set ssr to false

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from '@nuxt/bridge'
 export default defineNuxtConfig({
   target: 'static',
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: true,
+  ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
