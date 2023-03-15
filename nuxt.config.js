1052599d4f847d09167f9c2bb5dbdf35d0c56db7
Kristute <r.kryste@gmail.com>
2023-03-15 09:07:59 +0200

    edit css config path

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default defineNuxtConfig({
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@/src/assets/css/main.css'],
+  css: ['~/assets/css/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
